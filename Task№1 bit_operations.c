c09c7584205b01251e7b403e1a61398f2106927a
AinWaV <91606710+AinWaV@users.noreply.github.com>
2021-11-04 12:56:53 +0700

    Update Task№1 bit_operations.c

--- Task№1 bit_operations.c
+++ Task№1 bit_operations.c
@@ -3,13 +3,13 @@
 int main() {
 	int array = 0x00DDAABB;
 	unsigned char out;
-	printf("initial array=%x\N", array); 
+	printf("initial array=%x\n", array); 
 	//Выводим байты числа array
 	for (int i = 0; i < 4; i++) {
 		out = ((array >> 8 * i) & 0x000000FF);
-		printf("%d byte array=%hhx\N", i+1, out);//%hhx - вывод 1 байтного числа, если исп %x, char неявно приводится к int
+		printf("%d byte array=%hhx\n", i+1, out);//%hhx - вывод 1 байтного числа, если исп %x, char неявно приводится к int
 	}
 	array ^= 0x00110000;// XOR-им 
-	printf("modified array=%x\N",array);
+	printf("modified array=%x\n",array);
 	return 0;
 }
