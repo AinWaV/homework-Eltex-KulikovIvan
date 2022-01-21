330ffb2db0ec4abcb6bb81fca4b38f575008ff36
AinWaV <91606710+AinWaV@users.noreply.github.com>
2022-01-22 00:23:19 +0700

    Delete arithmetic_operations.h

--- Task_7_dynamicLib_plugins/arithmetic_operations.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-int add(const int value1, const int value2);
-
-int subtract(const int value1, const int value2);
-
-int multiply(const int value1, const int value2);
-
-int divide(const int value1, const int value2);
